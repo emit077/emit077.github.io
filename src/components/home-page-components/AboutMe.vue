19ab2eebba38f7f1cd1dca6f76a3729cb62401b4
amit.sahu <amit.sahu@codenicely.in>
2022-08-15 17:27:59 +0530

    added resume

--- src/components/home-page-components/AboutMe.vue
+++ src/components/home-page-components/AboutMe.vue
@@ -1,15 +1,46 @@
 <template>
   <div class="app-padding py-15 my-15" id="about-me">
     <p class="shadow-text">About me</p>
-    <qube-outline class="spin-animation float-right" :opacity="0.7" :stroke="'#F3B24A'" style="width: 15px"/>
-    <div class="" :class="$vuetify.display.mobile?'mt-n2':'mt-n15'">
-      <common-content-wrapper :title="title" :subtitle="subtitle" :description="description"/>
-    </div>
+<!--    <v-row align="start" justify="start" no-gutters>-->
+<!--      <v-col xs="12" sm="6" md="9" class="justify-center text-stroke-1 pr-6 py-6">-->
+        <qube-outline class="spin-animation float-right" :opacity="0.7" :stroke="'#F3B24A'" style="width: 15px"/>
+        <div class="" :class="$vuetify.display.mobile?'mt-n2':'mt-n15'">
+          <common-content-wrapper :title="title" :subtitle="subtitle" :description="description"/>
+        </div>
+<!--      </v-col>-->
+<!--      <v-col xs="12" sm="6" md="3" class="text-center text-stroke-1 pr-6 py-6">-->
+<!--        <img v-if="$store.state.currentTheme=='dark'" :src="require('@/assets/images/dark/profile-img.png')" alt=""-->
+<!--             class="profile-img">-->
+<!--        <img v-else :src="require('@/assets/images/light/profile-img.png')" alt="" class="profile-img">-->
+
+        <a :href="pdfLink" download="download" class="float-right">
+          <button class="get-cv-btn mt-7">
+            Download CV
+          </button>
+        </a>
+<!--      </v-col>-->
+<!--    </v-row>-->
   </div>
 </template>
 <!--  -->
 <style lang="scss" scoped>
+.profile-img {
+  width: 100%;
+}
 
+.get-cv-btn {
+  padding: 5px 15px;
+  border-radius: 8px;
+  border: solid 1.5px rgb(var(--v-theme-stroke-2), 0.8);
+  color: rgb(var(--v-theme-stroke-2), 0.8);
+  background-color: rgb(var(--v-theme-stroke-2), 0.1);
+  transition: background-color 0.5s linear, color 0.5s linear;
+}
+
+.get-cv-btn:hover {
+  color: rgb(var(--v-theme-stroke-1), 0.8);
+  background-color: rgb(var(--v-theme-stroke-2), 1);
+}
 </style>
 <!--   -->
 <script>
@@ -24,6 +55,7 @@ export default {
   },
   data() {
     return {
+     publicPath: process.env.BASE_URL,
       title: "About me",
       subtitle: "Know more about me",
       description: "Hello! My name is Amit Kumar Sahu. I'm a software developer & UI/UX Designer based in Raipur Chhattisgarh. I enjoy creating things that live on the internet. It all started from my college days when I start building interest in software development and decided to try editing custom themes and build a website for a college event, Fast-forward to today, I had the privilege of working at a start-up and a huge corporation. " +
--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,17 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 
   pluginOptions: {
     vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    }
+  },
+  chainWebpack: config => {
+    config.module
+      .rule("pdf")
+      .test(/\.pdf$/)
+      .use("file-loader")
+      .loader("file-loader");
   }
 })
