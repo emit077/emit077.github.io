1dc56622d59f76113b482cd2220801adf43d1d28
amit.sahu <amit.sahu@codenicely.in>
2022-10-20 15:25:14 +0530

    experince is auto incrimenting

--- src/components/home-page-components/MyExperience.vue
+++ src/components/home-page-components/MyExperience.vue
@@ -5,7 +5,7 @@
     <div class="d-flex justify-space-around" v-if="!$vuetify.display.mobile">
       <div class="d-inline-flex">
         <div class="d-inline-flex  font-weight-bold text-stroke-2"
-             :class="$vuetify.display.mobile?'text-h2':' text-h1'"> 3.6
+             :class="$vuetify.display.mobile?'text-h2':' text-h1'"> {{ experience }}
         </div>
         <div class="d-inline-flex align-center pl-2 text-stroke-1">Years of<br>Excellence
         </div>
@@ -17,7 +17,7 @@
     </div>
     <div v-else class="text-center">
       <div class=" ">
-       <div class="text-h1 font-weight-bold text-stroke-2">3.6</div>
+        <div class="text-h1 font-weight-bold text-stroke-2">3.6</div>
         <div class=" align-center pl-2 text-stroke-1">Years of Excellence</div>
       </div>
       <div class="mt-10">
@@ -45,12 +45,23 @@ export default {
     return {
       title: "Experience",
       subtitle: "Product designer and Developer, Based in Raipur",
-      description: "Passionate about visual design and technology. I specialized in frontend development and UI/UX design and my passion is about building elegant and professional user interfaces and websites."
+      description: "Passionate about visual design and technology. I specialized in frontend development and UI/UX design and my passion is about building elegant and professional user interfaces and websites.",
+      experience: "",
     }
   },
   mounted() {
-
+    this.calcExperience()
   },
-  methods: {}
+  methods: {
+    calcExperience() {
+      var start_date = new Date("6/1/2019");
+      var current_date = new Date();
+      var diffDays = parseInt((current_date - start_date) / (1000 * 60 * 60 * 24), 10);
+      let diffYears = parseInt(diffDays / 365)
+      let diffMonths = parseInt((diffDays % 365) / 30)
+      this.experience = diffYears + "." + diffMonths
+      console.log(diffDays, diffYears, diffMonths)
+    }
+  }
 };
 </script>
